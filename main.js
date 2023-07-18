079c6f73900dd8502a1d37693bc8f8f912a0ece0
Luiza Ribeiro <luiza@oneweb.com.br>
2023-07-18 15:51:32 -0300

    mais adds

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 const loadingElement = document.querySelector("#loading");
 const postsContainer = document.querySelector(".posts-container");
 
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+// Id from URL
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+// All Posts
+
 async function getAllPosts() {
     const response = await fetch(url);
 
@@ -24,9 +35,38 @@ async function getAllPosts() {
         div.appendChild(title);
         div.appendChild(body);
         div.appendChild(link);
-
         postsContainer.appendChild(div);
     });
 }
 
-getAllPosts();
+// Individual Post
+
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`),
+    ]);
+
+    const dataPost = await responsePost.json();
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    dataComments.map((comment) => )
+}
+
+if (!postId) {
+    getAllPosts();
+} else {
+    getPost(postId);
+}
